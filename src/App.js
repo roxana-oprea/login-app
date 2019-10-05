33361760f1ee0af9b4600af5809508a435de4a8c
Roxana Oprea <oprearoxanaadolfina@gmail.com>
2019-10-05 18:42:11 +0300

    add logic for register user

--- src/App.js
+++ src/App.js
@@ -2,19 +2,47 @@ import React from 'react';
 import Title from './Title';
 import Form from './Form';
 import CompanyName from './CompanyDescription';
+import Registered from './Registered';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="company-description">
-        <CompanyName />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isRegistered: false,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleSubmit() {
+    this.setState({
+      isRegistered: true,
+    });
+  }
+
+  handleClick() {
+    this.setState({
+      isRegistered: false,
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="company-description">
+          <CompanyName />
+        </div>
+        {this.state.isRegistered ? (
+          <Registered handleClick={this.handleClick} />
+        ) : (
+          <div className="register">
+            <Title />
+            <Form handleSubmit={this.handleSubmit} />
+          </div>
+        )}
       </div>
-      <div className="login">
-        <Title />
-        <Form />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css';
 
-const Form = () => {
+const Form = props => {
+  const [fullName, setFullName] = useState('');
+  const [companyName, setCompanyName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <form className="form-login">
+    <form className="form-register" onSubmit={props.handleSubmit}>
       <div className="form-row">
         <label htmlFor="full-name">Name</label>
-        <input id="full-name" type="text" required />
+        <input
+          id="full-name"
+          type="text"
+          name="fullName"
+          value={fullName}
+          onChange={e => setFullName(e.target.value)}
+          required
+        />
       </div>
 
       <div className="form-row">
         <label htmlFor="company-name">Company Name</label>
-        <input id="company-name" type="text" required />
+        <input
+          id="company-name"
+          type="text"
+          name="companyName"
+          value={companyName}
+          onChange={e => setCompanyName(e.target.value)}
+          required
+        />
       </div>
 
       <div className="form-row">
         <label htmlFor="email">Email Address</label>
-        <input id="email" type="email" required />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          required
+        />
       </div>
 
       <div className="form-row">
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" required />
+        <input
+          id="password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
       </div>
 
       <div className="form-row">
